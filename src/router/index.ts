2748a8d81c75106603006d7958d17737bcfc3b1f
Carlos Henrique Almeida Oliveira <carlos.h.almeida@animaeducacao.com.br>
2023-07-25 23:20:42 -0300

    Adicionando View Admin, ico Athena e componnete formulario geneico para cadastro de cursos

--- src/router/index.ts
+++ src/router/index.ts
@@ -54,6 +54,11 @@ const routes = [
         path: '/login',
         name: 'Login',
         component: () => import('@/views/Login.vue'),
+      },
+      {
+        path: '/admin',
+        name: 'Admin',
+        component: () => import('@/views/Admin.vue'),
       }
     ],
   },
