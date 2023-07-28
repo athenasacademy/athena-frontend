88edb9099891881240e65ded4c639c94dffcb6e9
Carlos Henrique Almeida Oliveira <carlos.h.almeida@animaeducacao.com.br>
2023-07-28 00:26:24 -0300

    Substituição do formulario generico pelo de adicionar curso no admin, e formatando layout das informações dos cursos

--- src/components/ClassroomDetails.vue
+++ src/components/ClassroomDetails.vue
@@ -1,23 +1,34 @@
 <template>
   <div>
     <template v-if="Array.isArray(cursos)">
-      <v-card v-if="cursos.length > 0">
-        <v-card-title>{{ cursos[0].nome }}</v-card-title>
-        <v-card-text>
-          <div><strong>ID:</strong> {{ cursos[0].id }}</div>
-          <div><strong>Descrição:</strong> {{ cursos[0].descricao }}</div>
-          <div><strong>Carga Horária:</strong> {{ cursos[0].cargaHoraria }}</div>
-          <div>
-            <strong>Disciplinas:</strong>
-            <ul>
-              <li v-for="disciplina in cursos[0].disciplinas" :key="disciplina.id">{{ disciplina.nome }}</li>
-            </ul>
-          </div>
-          <div><strong>Área de Conhecimento:</strong> {{ cursos[0].areaConhecimento.nome }}</div>
-          <div><strong>Data de Cadastro:</strong> {{ cursos[0].dataCadastro }}</div>
-          <div><strong>Data de Atualização:</strong> {{ cursos[0].dataAtualizacao || 'N/A' }}</div>
-          <div><strong>Status:</strong> {{ cursos[0].ativo ? 'Ativo' : 'Inativo' }}</div>
-        </v-card-text>
+      <v-card elevation="6" v-if="cursos.length > 0">
+        <v-col>
+          <v-card-title>{{ cursos[0].nome }}</v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col cols="4">
+                <div><strong>ID:</strong> {{ cursos[0].id }}</div>
+                <div><strong>Descrição:</strong> {{ cursos[0].descricao }}</div>
+                <div><strong>Carga Horária:</strong> {{ cursos[0].cargaHoraria }}</div>
+                <div>
+                  <strong>Disciplinas:</strong>
+                  <ul>
+                    <li v-for="disciplina in cursos[0].disciplinas" :key="disciplina.id">{{ disciplina.nome }}</li>
+                  </ul>
+                </div>
+              </v-col>
+              <v-col cols="4">
+                <div><strong>Área de Conhecimento:</strong> {{ cursos[0].areaConhecimento.nome }}</div>
+                <div><strong>Data de Cadastro:</strong> {{ cursos[0].dataCadastro }}</div>
+                <div><strong>Data de Atualização:</strong> {{ cursos[0].dataAtualizacao || 'N/A' }}</div>
+                <div><strong>Status:</strong> {{ cursos[0].ativo ? 'Ativo' : 'Inativo' }}</div>
+              </v-col>
+              <v-col cols="4">
+                <img class="curse-ico" :src="cursoUm" alt="Ico curso 1" />
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-col>
       </v-card>
       <v-card v-else>
         <p>Nenhum curso disponível.</p>
@@ -27,19 +38,28 @@
       <v-card v-if="cursos">
         <v-card-title>{{ cursos.nome }}</v-card-title>
         <v-card-text>
-          <div><strong>ID:</strong> {{ cursos.id }}</div>
-          <div><strong>Descrição:</strong> {{ cursos.descricao }}</div>
-          <div><strong>Carga Horária:</strong> {{ cursos.cargaHoraria }}</div>
-          <div>
-            <strong>Disciplinas:</strong>
-            <ul>
-              <li v-for="disciplina in cursos.disciplinas" :key="disciplina.id">{{ disciplina.nome }}</li>
-            </ul>
-          </div>
-          <div><strong>Área de Conhecimento:</strong> {{ cursos.areaConhecimento.nome }}</div>
-          <div><strong>Data de Cadastro:</strong> {{ cursos.dataCadastro }}</div>
-          <div><strong>Data de Atualização:</strong> {{ cursos.dataAtualizacao || 'N/A' }}</div>
-          <div><strong>Status:</strong> {{ cursos.ativo ? 'Ativo' : 'Inativo' }}</div>
+          <v-row>
+            <v-col cols="4">
+              <div><strong>ID:</strong> {{ cursos.id }}</div>
+              <div><strong>Descrição:</strong> {{ cursos.descricao }}</div>
+              <div><strong>Carga Horária:</strong> {{ cursos.cargaHoraria }}</div>
+              <div>
+                <strong>Disciplinas:</strong>
+                <ul>
+                  <li v-for="disciplina in cursos.disciplinas" :key="disciplina.id">{{ disciplina.nome }}</li>
+                </ul>
+              </div>
+            </v-col>
+            <v-col cols="4">
+              <div><strong>Área de Conhecimento:</strong> {{ cursos.areaConhecimento.nome }}</div>
+              <div><strong>Data de Cadastro:</strong> {{ cursos.dataCadastro }}</div>
+              <div><strong>Data de Atualização:</strong> {{ cursos.dataAtualizacao || 'N/A' }}</div>
+              <div><strong>Status:</strong> {{ cursos.ativo ? 'Ativo' : 'Inativo' }}</div>
+            </v-col>
+            <v-col cols="4">
+              <img class="curse-ico" :src="cursoUm" alt="Ico curso 1" />
+            </v-col>
+          </v-row>
         </v-card-text>
       </v-card>
       <v-card v-else>
@@ -63,5 +83,10 @@ export default {
   setup(props) {
     return { cursos: props.cursosData };
   },
+  data() {
+    return {
+      cursoUm: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZK9F9QO0nrHw33phjOHzFw8DLUg4Rg8JchEE37ooG6KqwYXUxvbqqbFPF_xt_TUdOsAs&usqp=CAU',
+    };
+  }
 };
 </script>
